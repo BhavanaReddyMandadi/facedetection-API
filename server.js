dcc1fe5d8c8004a91fb607b0e7645c6e80043282
BhavanaMandadi <bhavanmandadi95@gmail.com>
2019-04-24 19:33:21 -0400

    changing

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-cylindrical-46140',
-    user : 'postgres',
-    password : 'M@dmad12',
-    database : 'smart-brain'
+    host : process.env.URL,
+   ssl : true,
   }
 });
 
